14b2088232bc0589b882d940bfefbf0c4127c068
sealzhk <sealzhk@users.noreply.github.com>
2020-10-13 23:36:42 +0600

    Initial Commit

---
+++ CatDogs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatDogs
+//
+//  Created by Alua Zhakieva on 10/13/20.
+//  Copyright © 2020 Alua Zhakieva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
